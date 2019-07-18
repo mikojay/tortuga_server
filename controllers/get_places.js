5027d783ed4480ac1116cb7ea23b42ed98d24548
Jascha Mikolajski <jaschi.miko@web.de>
2019-07-18 10:38:02 +0700

    controllers place and category

---
+++ controllers/get_places.js
@@ -0,0 +1,19 @@
+const db_place = require('../models/place')
+
+module.exports = (req, res) => {
+
+// let q = {}
+// if (req.query && req.query.category) {
+// 	q.category = req.query.category
+// }
+	db_place.find().sort('-date').populate({
+		path: 'category'
+	}).populate({
+		path: 'author',
+		select: 'id name'
+	}).then((data) => {
+		res.send(data)
+	}).catch((err) => {
+		res.send(err)
+	})
+}
